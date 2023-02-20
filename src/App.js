3ec0b2c4c2e20fac3aed97ff2cb41498b3971d6a
Arturo Mendes <arturomendesrobleda@gmail.com>
2023-02-20 23:38:10 +0100

    use circular viz

--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import './App.css'
 import VisualizerSpace from './components/VisualizerSpace/VisualizerSpace'
 import useDeviceAudio from './hooks/useDeviceAudio'
 
+const colorDict = {}
+const redMultiplier = Math.random()
+const greenMultiplier = Math.random()
+const blueMultiplier = Math.random()
+
+const getColorForFrequency = (frequency) => {
+  if (colorDict[frequency] == undefined) {
+    let red = (frequency + 1) * redMultiplier
+    let green = (frequency + 1) * greenMultiplier
+    let blue = (frequency + 1) * blueMultiplier
+
+    colorDict[frequency] = `${red}, ${green}, ${blue}`
+  }
+
+  return colorDict[frequency]
+}
+
 function App() {
   const analyser = useDeviceAudio()
   const audioSpaceRef = useRef()
@@ -11,66 +28,105 @@ function App() {
     if (analyser) {
       const canvasCtx = audioSpaceRef.current.getContext('2d')
 
-      let onAudio = () => {
-        requestAnimationFrame(onAudio)
+      let onAudioMakeCircles = () => {
+        requestAnimationFrame(onAudioMakeCircles)
         const canvasWidth = audioSpaceRef.current.width
         const canvasHeight = audioSpaceRef.current.height
 
         const bufferLength = analyser.frequencyBinCount
         const dataArray = new Uint8Array(bufferLength)
-
-        let pixelsPerSquare = Math.floor(
-          (canvasHeight * canvasWidth) / bufferLength
-        )
-
-        const baseSide = Math.sqrt(pixelsPerSquare)
-
-        const rows = Math.floor(canvasHeight / baseSide)
-        const rectHeight = canvasHeight / rows
-
-        const columns = Math.floor(bufferLength / rows)
-        const rectWidth = canvasWidth / columns
-
-        analyser.getByteFrequencyData(dataArray)
+        const maxRadius = Math.min(canvasWidth, canvasHeight) / 2
+        const centerY = canvasHeight / 2
+        const centerX = canvasWidth / 2
 
         canvasCtx.clearRect(0, 0, canvasWidth, canvasHeight)
 
-        // INFO: the loop defines how the frequencies are populated
+        analyser.getByteFrequencyData(dataArray)
+
         let frequency = 0
-        for (let i = 0; i < rows; i++) {
-          for (let j = 0; j < columns; j++) {
-            let rectY = i * rectHeight
-            let rectX = j * rectWidth
+        let radius = 1
+        const radiusStep = Math.round(maxRadius / bufferLength) || 1
 
-            // TODO: use audioInput value in a better way
-            let decibelValue = dataArray[frequency]
+        // TODO: make it look like disipating circles
 
-            // INFO: creates a grayscale representation 
-            let red = (frequency + 1) * 1
-            let green = (frequency + 1) * 1
-            let blue = (frequency + 1) * 1
+        for (let i = 0; i < bufferLength; i++) {
+          let decibelValue = dataArray[frequency]
 
-            canvasCtx.fillStyle = `rgba(${red}, ${green}, ${blue}, ${
-              decibelValue / 100
-            })`
-            canvasCtx.fillRect(rectX, rectY, rectWidth, rectHeight)
+          canvasCtx.beginPath()
+          canvasCtx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false)
+          canvasCtx.lineWidth = 1
+          canvasCtx.strokeStyle = `rgba(${getColorForFrequency(frequency)}, ${
+            decibelValue / 100
+          })`
+          canvasCtx.stroke()
 
-            if (process.env.NODE_ENV !== 'production') {
-              canvasCtx.textAlign = 'center'
-              canvasCtx.fillStyle = `rgb(255, 255, 255)`
-              canvasCtx.fillText(
-                `${decibelValue}`,
-                rectX + rectWidth / 2,
-                rectY + rectHeight / 2
-              )
-            }
-
-            frequency++
-          }
+          radius += radiusStep
+          frequency++
         }
       }
 
-      onAudio()
+      // let onAudioMakeScreen = () => {
+      //   requestAnimationFrame(onAudioMakeScreen)
+      //   const canvasWidth = audioSpaceRef.current.width
+      //   const canvasHeight = audioSpaceRef.current.height
+
+      //   const bufferLength = analyser.frequencyBinCount
+      //   const dataArray = new Uint8Array(bufferLength)
+
+      //   let pixelsPerSquare = Math.floor(
+      //     (canvasHeight * canvasWidth) / bufferLength
+      //   )
+
+      //   const baseSide = Math.sqrt(pixelsPerSquare)
+
+      //   const rows = Math.floor(canvasHeight / baseSide)
+      //   const rectHeight = canvasHeight / rows
+
+      //   const columns = Math.floor(bufferLength / rows)
+      //   const rectWidth = canvasWidth / columns
+
+      //   analyser.getByteFrequencyData(dataArray)
+
+      //   canvasCtx.clearRect(0, 0, canvasWidth, canvasHeight)
+
+      //   // INFO: the loop defines how the frequencies are populated
+      //   let frequency = 0
+      //   for (let i = 0; i < rows; i++) {
+      //     for (let j = 0; j < columns; j++) {
+      //       let rectY = i * rectHeight
+      //       let rectX = j * rectWidth
+
+      //       // TODO: use audioInput value in a better way
+      //       let decibelValue = dataArray[frequency]
+
+      //       // INFO: creates a grayscale representation
+      //       let red = (frequency + 1) * 1
+      //       let green = (frequency + 1) * 1
+      //       let blue = (frequency + 1) * 1
+
+      //       canvasCtx.fillStyle = `rgba(${red}, ${green}, ${blue}, ${
+      //         decibelValue / 100
+      //       })`
+      //       canvasCtx.fillRect(rectX, rectY, rectWidth, rectHeight)
+
+      //       if (process.env.NODE_ENV !== 'production') {
+      //         canvasCtx.textAlign = 'center'
+      //         canvasCtx.fillStyle = `rgb(255, 255, 255)`
+      //         canvasCtx.fillText(
+      //           `${decibelValue}`,
+      //           rectX + rectWidth / 2,
+      //           rectY + rectHeight / 2
+      //         )
+      //       }
+
+      //       frequency++
+      //     }
+      //   }
+      // }
+
+      // onAudioMakeScreen()
+
+      onAudioMakeCircles()
     }
   }, [analyser])
 
